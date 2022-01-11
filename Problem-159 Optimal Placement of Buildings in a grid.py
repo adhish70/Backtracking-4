c78ad45a7bcf514ea795349f1ba44cb0efd57186
Adhish Shrivastava <adhish70@gmail.com>
2022-01-11 09:53:49 -0500

    Initial Commit

---
+++ Problem-159 Optimal Placement of Buildings in a grid.py
@@ -0,0 +1,10 @@
+# Optimal Placement of Buildings in a grid
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class BuildingPlanner:
+    def minDist(self, height, width, buildings):
+        pass
---
+++ Problem-160 Brace Expansion.py
@@ -0,0 +1,11 @@
+# 1087. Brace Expansion
+# https://leetcode.com/problems/brace-expansion/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def expand(self, s: str) -> List[str]:
+        pass
