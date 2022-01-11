69922dff545f6229f795ff25b52260828e2fc2bb
Adhish Shrivastava <adhish70@gmail.com>
2022-01-11 10:08:06 -0500

    Problem 160 done

--- Problem-160 Brace Expansion.py
+++ Problem-160 Brace Expansion.py
@@ -1,11 +1,49 @@
 # 1087. Brace Expansion
 # https://leetcode.com/problems/brace-expansion/
 
-# Logic: 
+# Logic: Covert the input into list of lists. Recurse over the processed list and backtrack.
 
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(k^(n/k)) [k is the avg length of words inside braces and n is the total length]
+# Space Complexity: O(n)
 
 class Solution:
-    def expand(self, s: str) -> List[str]:
-        pass
+    def _helper(self, blocks, idx, curPath, result):
+        # Base Case
+        if idx == len(blocks):
+            result.append("".join(curPath))
+            return
+        
+        for j in range(len(blocks[idx])):
+            curPath.append(blocks[idx][j])
+
+            self._helper(blocks, idx + 1, curPath, result)
+
+            curPath.pop()
+
+    def expand(self, s: str):
+        # Preprocess: Convert input into list of list
+        blocks = []
+        i = 0
+
+        while i < len(s):
+            temp = []
+            if s[i] == '{':
+                i += 1
+                while s[i] != '}':
+                    if s[i] != ',':
+                        temp.append(s[i])
+                    i += 1
+            else:
+                temp.append(s[i])
+            blocks.append(temp)
+            i += 1
+        
+        result = []
+        self._helper(blocks, 0, [], result)
+
+        return result
+
+
+obj = Solution()
+print(obj.expand('{a,b}c{d,e}f'))
+print(obj.expand('abcde'))
